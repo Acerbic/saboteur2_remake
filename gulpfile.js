5fb48e8b2ae33a62d53c4595a8cde52c3bac4fd6
Anthony Cerbic <kuzzdra@gmail.com>
2018-10-07 07:06:09 +0300

    basic twig page rendering

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,48 @@
 var gulp = require('gulp');
 
-gulp.task('compile', function (done) {
+gulp.task('compile', function () {
     'use strict';
     var twig = require('gulp-twig');
     return gulp.src('./source/index.twig')
         .pipe(twig({
             data: {
                 title: 'Gulp and Twig',
-                benefits: [
-                    'Fast',
-                    'Flexible',
-                    'Secure'
+                footer_note: 'Some foot notes',
+                top_pages: [
+                    {
+                        name: 'one',
+                        url: 'index.html'
+                    },
+                    {
+                        name: 'two',
+                        url: 'index.html'
+                    },
+                    {
+                        name: 'three',
+                        url: 'index.html'
+                    }
                 ]
             }
         }))
         .pipe(gulp.dest('./public'));
 });
 
-gulp.task('default', gulp.series('compile'));
+gulp.task('copy-images', function() {
+    'use strict';
+    return gulp.src('./source/images/*')
+        .pipe(gulp.dest('./public/images'))
+});
+
+gulp.task('copy-reset-css', function () {
+    'use strict';
+    return gulp.src('./node_modules/reset-css/reset.css')
+        .pipe(gulp.dest('./public'))
+})
+
+gulp.task('process-css', function () {
+    'use strict';
+    return gulp.src('./source/style.css')
+        .pipe(gulp.dest('./public'))
+})
+
+gulp.task('default', gulp.series('compile', 'copy-images', 'copy-reset-css', 'process-css'));
