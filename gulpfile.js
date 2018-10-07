412485d426626115ccebc35160a78304934cb929
Anthony Cerbic <kuzzdra@gmail.com>
2018-10-07 08:28:49 +0300

    more twig compilation. main page built

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,30 @@ var gulp = require('gulp');
 
 gulp.task('compile', function () {
     'use strict';
+
+    // load twig compiler
     var twig = require('gulp-twig');
-    return gulp.src('./source/index.twig')
+
+    // list of pages for this website
+    var pages = require('./pages.json');
+
+    // get all pages that need to be compiled with Twig
+    var sources = pages.map(entry => './source/pages/' + entry.src);
+
+    // build menu entries for main navigation
+    var menu = pages.filter(entry => entry.name && entry.name.length)
+        .map(entry => ({
+            name: entry.name,
+            url: entry.src.replace(/\.twig$/, '.html')
+        }));
+
+    // start compiling
+    return gulp.src(sources)
         .pipe(twig({
             data: {
                 title: 'Gulp and Twig',
                 footer_note: 'Some foot notes',
-                top_pages: [
-                    {
-                        name: 'one',
-                        url: 'index.html'
-                    },
-                    {
-                        name: 'two',
-                        url: 'index.html'
-                    },
-                    {
-                        name: 'three',
-                        url: 'index.html'
-                    }
-                ]
+                top_pages: menu
             }
         }))
         .pipe(gulp.dest('./public'));
