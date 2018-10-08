b76883f438931afd67a1ba71b3b65a139d39d561
Anthony Cerbic <kuzzdra@gmail.com>
2018-10-08 06:23:23 +0300

    create gulp watch to observe menu changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,47 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var twig = require('gulp-twig');
+var data = require('gulp-data');
+var fs = require('fs');
 
 /**
- * Returns a gulp plugin prepared for compiling a twig page
+ * Load and prepare main nav menu data
  */
-function createPageCompiler() {
+function getMenuData() {
     'use strict';
 
-    // list of pages for this website
-    var menu_data = require('./mainmenu.json');
-
-    // build menu entries for main navigation
-    var menu = menu_data.filter(entry => entry.name && entry.name.length)
-        .map(entry => ({
-            name: entry.name,
-            url: entry.src.replace(/\.twig$/, '.html')
-        }));
-
-    return twig({
-        data: {
-            title: 'Default title',
-            footer_note: 'Default footer',
-            top_pages: menu
-        }
-    });
+    var menu_data = JSON.parse(fs.readFileSync('./mainmenu.json'))
+                        .map(entry => ({
+                            name: entry.name,
+                            url: entry.src.replace(/\.twig$/, '.html')
+                        }));
+    return {top_pages: menu_data};
 }
 
 gulp.task('watch-pages', function () {
     'use strict';
 
     // Endless stream mode
-    watch('./source/pages/*.twig', { ignoreInitial: false, verbose: true })
-        .pipe(createPageCompiler())
+    return watch('./source/pages/*.twig', { ignoreInitial: false, verbose: true })
+        .pipe(data(getMenuData))
+        .pipe(twig())
         .pipe(gulp.dest('./public'));
 });
 
+gulp.task('watch-menu', function () {
+    'use strict';
+
+    return watch('./mainmenu.json', { verbose: true }, gulp.task('compile'));
+});
+
+
 gulp.task('watch', gulp.parallel(
     'watch-pages',
-    function () {
-        // watch css
-        watch('./source/*.css', { ignoreInitial: false, verbose: true }, gulp.task('process-css'));
+    'watch-menu',
+    function watchCss() {
+        'use strict';
+
+        return watch('./source/*.css', { ignoreInitial: false, verbose: true }, gulp.task('process-css'));
     })
 );
 
@@ -52,18 +53,21 @@ gulp.task('compile', function () {
 
     // start compiling
     return gulp.src('./source/pages/*.twig')
-        .pipe(createPageCompiler())
+        .pipe(data(getMenuData))
+        .pipe(twig())
         .pipe(gulp.dest('./public'));
 });
 
 gulp.task('copy-images', function() {
     'use strict';
+
     return gulp.src('./source/images/*')
         .pipe(gulp.dest('./public/images'))
 });
 
 gulp.task('process-css', function () {
     'use strict';
+
     return gulp.src(['./source/style.css', './node_modules/reset-css/reset.css'])
         .pipe(gulp.dest('./public'))
 })
