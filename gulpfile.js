6a18f6deb775981f1de8b7386b5255afe7113c1e
Anthony Cerbic <kuzzdra@gmail.com>
2018-10-16 17:54:38 +0300

    comments!

--- gulpfile.js
+++ gulpfile.js
@@ -18,16 +18,24 @@ function getMenuData() {
     return {top_pages: menu_data};
 }
 
+/**
+ * Watch individual pages and recompile as needed (only the changed page is 
+ * recompiled)
+ */
 gulp.task('watch-pages', function () {
     'use strict';
 
-    // Endless stream mode (recompile individual pages)
+    // Endless stream mode
     return watch('./source/pages/*.twig', { ignoreInitial: false, verbose: true })
         .pipe(data(getMenuData))
         .pipe(twig())
         .pipe(gulp.dest('./public'));
 });
 
+/**
+ * Watch additional resources - twig components, menu listing.
+ * The whole project is recompiled on change
+ */
 gulp.task('watch-twig-extra', function () {
     'use strict';
 
@@ -38,19 +46,26 @@ gulp.task('watch-twig-extra', function () {
     );
 });
 
-
+/**
+ * Group task that starts all watch tasks for the project
+ */
 gulp.task('watch', gulp.parallel(
     'watch-pages',
     'watch-twig-extra',
+    // inlined task - watch css file
     function watchCss() {
         'use strict';
 
-        return watch('./source/*.css', { ignoreInitial: false, verbose: true }, gulp.task('process-css'));
+        return watch(
+            './source/*.css',
+            { ignoreInitial: false, verbose: true },
+            gulp.task('process-css')
+        );
     })
 );
 
 /**
- * Process all twig files
+ * Compile all *.twig page templates into *.html files
  */
 gulp.task('compile', function () {
     'use strict';
@@ -62,13 +77,22 @@ gulp.task('compile', function () {
         .pipe(gulp.dest('./public'));
 });
 
+/**
+ * Copy static files into results directory - ready for deployment
+ */
 gulp.task('copy', function() {
     'use strict';
 
-    return gulp.src(['./source/images/**/*', './source/audio/**/*'], {base: './source'})
+    return gulp.src(
+            ['./source/images/**/*', './source/audio/**/*'],
+            {base: './source'}
+        )
         .pipe(gulp.dest('./public'))
 });
 
+/**
+ * Do CSS processing, as needed, and put output into results directory
+ */
 gulp.task('process-css', function () {
     'use strict';
 
@@ -76,4 +100,9 @@ gulp.task('process-css', function () {
         .pipe(gulp.dest('./public'))
 })
 
+/**
+ * Default task - full build of the project. The result of this task
+ * is a './public' directory, containing static *.html files, and all resources
+ * used by the website, ready to be deployed.
+ */
 gulp.task('default', gulp.parallel('compile', 'copy', 'process-css'));
